3cf7a9602f0ea081deb4892d19e64b25dddd0597
Santhoshkumar Nagulanchi <santhoshkumarnagulanchi@Santhoshkumars-MacBook-Pro.local>
2023-05-07 09:40:30 +0800

    added cloudbus refresh with manual kafka topic

--- Notification Service/src/main/java/com/vignette/eis/mega/controllers/NotificationController.java
+++ Notification Service/src/main/java/com/vignette/eis/mega/controllers/NotificationController.java
@@ -1,10 +1,12 @@
 package com.vignette.eis.mega.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class NotificationController {
 
     @Value("${model}")
