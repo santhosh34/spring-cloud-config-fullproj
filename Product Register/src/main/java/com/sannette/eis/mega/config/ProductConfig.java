a5eb879d7384097ea7d3ae60972409e3aa560db8
Santhoshkumar Nagulanchi <santhoshkumarnagulanchi@Santhoshkumars-MacBook-Pro.local>
2023-05-07 09:47:52 +0800

    added cloudbus refresh with manual kafka topic

--- Product Register/src/main/java/com/sannette/eis/mega/config/ProductConfig.java
+++ Product Register/src/main/java/com/sannette/eis/mega/config/ProductConfig.java
@@ -3,10 +3,12 @@ package com.sannette.eis.mega.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Data
+@RefreshScope
 public class ProductConfig {
     @Value("${model}")
     private String model;
